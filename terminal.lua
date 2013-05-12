780b6837b57aedf3b9aa92e409c61875ea161def
lordcirth <lordcirth@gmail.com>
2013-05-12 16:21:24 -0400

    Split into keyb and terminal

--- terminal.lua
+++ terminal.lua
@@ -1,6 +1,29 @@
-digipad.terminal_formspec =
+digipad.keyb_formspec =
 "size[4,1;]"..
 "field[0,1;4,1;input;Input;]"
+
+minetest.register_node("digipad:keyb", {
+	description = "Digiline keyboard",
+	walkable = true,
+	digiline = 
+		{
+			receptor={},
+			effector={},
+		},
+		groups = {dig_immediate = 2},
+		on_construct = function(pos)
+			local meta = minetest.env:get_meta(pos)
+			meta:set_string("formspec", digipad.terminal_formspec)
+		end,
+		on_receive_fields = function(pos, formname, fields, sender)
+			local text = fields.input
+			local channel = "tty1"
+			digiline:receptor_send(pos, digiline.rules.default, channel, text)
+		end,
+		
+
+})
+
 
 minetest.register_node("digipad:terminal", {
 	description = "Text entry terminal",
